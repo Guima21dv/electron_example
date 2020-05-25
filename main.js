78438250caaa20648cc427198ec6ef78e45018e7
Guilherme <guima_dev@hotmail.com>
2020-05-25 19:42:02 -0300

    Adicionado electron e inciado o projeto

---
+++ main.js
@@ -0,0 +1,31 @@
+const {app, BrowserWindow} = require('electron')
+
+let win
+
+function createWindow(){
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        },
+        alwaysOnTop: true,
+        darkTheme: true,
+    })
+
+    win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if( BrowserWindow.getAllWindows().length === 0){
+        createWindows()
+    }
+})
